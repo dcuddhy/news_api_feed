d380ddb17541ac745cb2dc7b6b1b0ab1234efb35
dcuddhy <dcuddhy@gmail.com>
2020-10-25 10:53:09 -0600

    fetch data, but use sample data for dev

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/data.json';
 
 function App() {
+  const [fetchedData, setFetchedData] = useState(data);
+
+  const apiKey = process.env.REACT_APP_API_KEY || '';
+  const apiEndpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+  
+  // TODO this works, but lets use sample data until we're cleaning up to make sure we don't hit an API limit
+  // useEffect(() => {
+  // fetch(apiEndpoint)
+  //   .then(response => response.json())
+  //   .then(data => setFetchedData(data.articles))
+  //   .catch((error) => {
+  //     console.error('FETCH ERROR: ', error);
+  //   });
+  // }, []);
+
+  console.log('fetchedData: ', fetchedData);
+
   return (
     <div className="App">
       <header className="App-header">
